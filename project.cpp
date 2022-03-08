e5b65f3ec845e17f00860a509531f92df71074df
Vanamnikhil <96713969+Vanamnikhil@users.noreply.github.com>
2022-03-08 08:36:29 +0530

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter user age:";
+    cin>>age;
+    if(age>17) {
+    cout<<"Eligible";
+    } else 
+    cout<<"not eligible";
+    return 0;
+}
